ee2113df4ca423cca4380452d68501ac42e52e21
saim <thanhhen2002@gmail.com>
2025-02-14 13:14:04 +0100

    [STRUCT]Creation de contact

---
+++ js/contact.js
@@ -0,0 +1,6 @@
+function showEmail() {
+    var emailDisplay = document.getElementById("emailDisplay");
+    var emailImage = document.getElementById("emailImage");
+    emailImage.style.display = "none"; // Masque l'image
+    emailDisplay.textContent = "cheabuon.saim@etu.uca.fr";
+}
